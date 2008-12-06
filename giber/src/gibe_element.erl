e7fb708ffb18f79264c3be8eeee0e3f584b4aff0
Tristan Sloughter <tristan@ir.iit.edu>
2008-12-05 21:02:30 -0600

    glug presentation ready

---
+++ giber/src/gibe_element.erl
@@ -0,0 +1,18 @@
+-module (gibe_element).
+-include ("wf.inc").
+-export ([update_gibes/1]).
+
+update_gibes (User) ->
+    Gibes = db_backend:get_all_gibes (User),
+    
+    lists:foreach (fun ({_K, Username, {{Y, M, D}, {Hour, Min, Sec}}, Gibe}) -> 
+                           Date = io_lib:format("~w~s~w~s~w~s~w~s~w~s~w", [M, "-", D, "-", Y, " at ", Hour, ":", Min, ":", Sec]),
+                           wf:insert_top (gibes, create_gibe_element(Username, Date, Gibe)) end, Gibes).
+
+create_gibe_element (Username, Date, Gibe) ->
+    FlashID = wf:temp_id(),
+    InnerPanel = #panel { class=flash, actions=#effect_blinddown 
+                          { target=FlashID, duration=0.4 }, 
+                          body=[
+                                #panel { class=flash_content, body=[ Gibe, #br{}, "from: ", Username, " on ", Date ] }]},
+    [#panel { id=FlashID, style="display: none;", body=InnerPanel }].
